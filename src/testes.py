8be2f9a734062109332c862fd9fce302cf857db5
Matheus Freitas <caroltirzah@hotmail.com>
2020-09-22 22:30:05 -0300

    ac2

--- src/testes.py
+++ src/testes.py
@@ -1,35 +1,35 @@
 import jogovelha
-
 import sys
 
 erroInicializar = False
-
 jogo = jogovelha.inicializar()
 
 if len(jogo) != 3:
-
-erroInicializar = True
-
+        erroInicializar = True
 else:
-
-for linha in jogo:
-
-if len(linha) != 3:
-
-erroInicializar = True
-
-else:
-
-for elemento in linha:
-
-if elemento != '.':
-
-erroInicializar = True
-
+        for linha in jogo:
+                if len(linha) != 3:
+                        erroInicializar = True
+                else:
+                        for elemento in linha:
+                                if elemento != '.':
+                                        erroInicializar = True
 if erroInicializar:
-
-sys.exit(1)
-
+        sys.exit(1)
 else:
+        sys.exit(0)
+def inicializar():
+	tab = [ ]
+	for i in range(3):
+		linha = [ ]
+		for j in range(3):
+			linha.append(".")
+		tab.append(linha)
+	return tab
 
-sys.exit(0)
+def main( ):
+	jogo = inicializar( )
+	print (jogo)
+
+if name == "_main_":
+	main()
